176abf6db6576b3ea6d66871682cc9f585f7c339
asdflkj169 <asdflkj169@gmail.com>
2024-11-24 21:17:50 +0900

    一旦library を表示できた。

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {NgxNekoLibraryComponent} from 'ngx-neko-library'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgxNekoLibraryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
